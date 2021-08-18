6933aa42f4adb8b30b953d832eb3a6e138b06688
sekharlee <51740389+sekharlee@users.noreply.github.com>
2021-08-19 03:55:06 +0530

    Update order_subdomains.sh

--- order_subdomains.sh
+++ order_subdomains.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-echo "Order.sh"
+
 
 cat $1|tr -d [:blank:]|perl -lne 'print join ".", reverse(split /\./)'|sort |awk -F. ' !seen[$1.$2.$3]++ && NR>1 {print ""} {for (i=NF;i>1;i--) printf "%s.", $i; print $1}'|xargs -n 1
