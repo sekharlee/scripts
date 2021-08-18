3230f5b88384f60160045d069f37b6966e2b75f1
sekharlee <51740389+sekharlee@users.noreply.github.com>
2021-08-19 03:49:17 +0530

    Create order_subdomains.sh

---
+++ order_subdomains.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+cat $1|tr -d [:blank:]|perl -lne 'print join ".", reverse(split /\./)'|sort |awk -F. ' !seen[$1.$2.$3]++ && NR>1 {print ""} {for (i=NF;i>1;i--) printf "%s.", $i; print $1} '
